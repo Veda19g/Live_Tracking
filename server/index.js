724f411e5c833809e8787f6b6467b0a17d378206
Veda19g <127167049+Veda19g@users.noreply.github.com>
2025-02-21 22:52:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: 'https://live-tracking-xi.vercel.app/' || '*' }));
+app.use(cors({ origin: 'https://live-tracking-n3syte67g-veda19gs-projects.vercel.app/' || '*' }));
 app.use(express.json()); // Parse JSON request bodies
 const server = http.createServer(app);
 const io = new Server(server, {
